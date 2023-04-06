3e3b6cd446387086f46e3bc95f654f0d884d6d4d
Baseadam <adam.fiedko@gmail.com>
2023-04-06 22:17:13 +0200

    feat: add age and description and also image

--- apps/adoption/src/cat-list-item/cat-list-item.tsx
+++ apps/adoption/src/cat-list-item/cat-list-item.tsx
@@ -3,8 +3,21 @@ import styles from './cat-list-item.module.css';
 
 interface CatListItemProps {
   name: string;
+  age: number;
+  breed: string;
+  color: string;
+  gender: string;
+  size: string;
+  image: string;
+  description: string;
+  adoption_fee: number;
 }
 
 export const CatListItem: FC<CatListItemProps> = (props) => {
-  return <div className={styles.asdf}>{props.name}</div>;
+  return (
+    <div className={styles.cats}>
+      {props.name} <br />Age: {props.age} <br />Description:  {props.description}
+      <img src={props.image} alt="głaskać kotki wciągać kreski"/>
+    </div>
+  );
 };
--- apps/adoption/src/cat-list/cat-list.tsx
+++ apps/adoption/src/cat-list/cat-list.tsx
@@ -5,7 +5,17 @@ export const CatList = () => {
   return (
     <div>
       {cats.cats.map((cat) => (
-        <CatListItem name={cat.name} />
+        <CatListItem 
+          name={cat.name}
+          age={cat.age}
+          breed={cat.breed}
+          color={cat.color}
+          gender={cat.gender}
+          size={cat.size}
+          image={cat.image}
+          description={cat.description}
+          adoption_fee={cat.adoption_fee}
+           />
       ))}
     </div>
   );
