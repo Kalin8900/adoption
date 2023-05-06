722c468c14cf2a9c71bdb31db613e69d6f7c774d
Baseadam <adam.fiedko@gmail.com>
2023-05-06 15:11:52 +0200

    learing routing

--- apps/adoption/src/api/getAllCats.ts
+++ apps/adoption/src/api/getAllCats.ts
@@ -1,22 +1,7 @@
-import React from 'react';
 import { cats } from './data.json';
+import { Cat } from "./getCatById"
 
-export interface Cat {
-  id: number;
-  name: string;
-  age: number;
-  breed: string;
-  color: string;
-  gender: string;
-  image: string;
-  description: string;
-  adoption_fee: number;
-  available: boolean;
-}
 
-export const getAllCats = () : Cat | null => {
-    
-    return (
-        
-    )
+export const getAllCats = () : Cat[] => {
+    return cats
 }
